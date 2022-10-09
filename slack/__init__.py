eed781d04775c79babfe575628bf5fd2b424e6fb
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-10-09 19:43:55 +0900

    FIX: import optimization.

--- slack/__init__.py
+++ slack/__init__.py
@@ -1,11 +1,10 @@
 from .channel import *
+from .member import *
 from .message import *
 from .team import *
 
-from .member import *
 from .route import *
 from .httpclient import *
-
 from .ws import *
 from .state import *
 from .client import *
