12ba56cf4e60848f93777491bc02aa64690b3109
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-10-12 19:56:21 +0900

    create docs.

---
+++ docs/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+sys.path.append(os.path.abspath("extensions"))
+
+project = 'slack.py'
+copyright = '2022, peco2282'
+author = 'peco2282'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "builder",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.napoleon",
+    "sphinxcontrib_trio",
+    "details",
+    "exception_hierarchy",
+    "attributetable",
+    "resourcelinks",
+    "nitpick_file_ignorer",
+    "myst_parser",
+]
+master_doc = "index"
+pygments_style = "friendly"
+
+source_suffix = {
+    ".rst": "restructuredtext",  # Used For The Other Docs
+    ".md": "markdown",  # Used ONLY In the Guide For Faster Making Time
+}
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+napoleon_numpy_docstring = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'basic'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. slack.py documentation master file, created by
+   sphinx-quickstart on Tue Oct 11 19:24:31 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to slack.py's documentation!
+====================================
+
+slack.py is a modern, easy to use, feature-rich, and async ready API wrapper
+for slack.
+
+**Features:**
+
+- Modern Pythonic API using ``async``\/``await`` syntax
+- Sane rate limit handling that prevents 429s
+- Command extension to aid with bot creation
+- Easy to use with an object oriented design
+- Optimised for both speed and memory
+
+.. toctree::
+   :maxdepth: 1
+
+   api
