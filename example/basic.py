724467143bb3d9e38e670e87b8a60ad3e634e735
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-11-15 20:23:54 +0900

    add example file

---
+++ example/basic.py
@@ -0,0 +1,33 @@
+import slack
+
+client = slack.Client(
+    user_token="...",
+    bot_token="...",
+    token="..."
+)
+
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+
+@client.event
+async def on_message(message: slack.Message):
+    if message.author.bot:
+        return
+
+    if message.content.startswith("!hello"):
+        await message.channel.send(f"Hello! {message.author.name}")
+
+@client.event
+async def on_channel_create(channel: slack.Channel):
+    await channel.send("Hello!")
+
+
+@client.event
+async def on_channel_unarchive(channel: slack.Channel, user: slack.Member):
+    await channel.send_as_user(f"{user.name} unarchive this channel.")
+
+
+client.run()
