b5df56d5c3e4c5909891d60d289947a3472a9585
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-11-16 09:38:37 +0900

    add new example

---
+++ example/subclassing.py
@@ -0,0 +1,28 @@
+import slack
+
+
+class MyClient(slack.Client):
+    def __init__(
+            self,
+            user_token: str = "xoxp-...",
+            bot_token: str = "xoxb-...",
+            token: str = "xapp-...",
+    ):
+        super().__init__(
+            user_token,
+            bot_token,
+            token,
+        )
+
+    async def on_ready(self):
+        print("Bot is ready.")
+
+    async def on_message(self, message: slack.Message):
+        if message.author.bot:
+            return
+
+        if message.content.startswith("!hello"):
+            await message.channel.send(f"Hello! {message.author.name}")
+
+    async def on_channel_create(self, channel: slack.Channel):
+        await channel.send("Hello!")
