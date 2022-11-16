f412dde931c2261ab41c4a232e9daa9c646fc317
peco2282 <87461070+peco2282@users.noreply.github.com>
2022-11-16 09:54:05 +0900

    FIX: easy to setup.

--- setup.py
+++ setup.py
@@ -1,8 +1,29 @@
+import re
+
 from setuptools import setup
+
+long_description = ''
+version = ''
+
+with open("README.md", encoding="utf8") as f:
+    long_description = f.read()
+
+with open("slack/__init__.py", encoding="utf8") as f:
+
+    search = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE)
+
+    if search is not None:
+        version = search.group(1)
+
+    else:
+        raise RuntimeError("Could not grab version string")
+
+if not version:
+    raise RuntimeError("version is not set")
 
 setup(
     name='wsslack.py',
-    version='1.0.0',
+    version=version,
     packages=[
         'slack',
         'slack.types'
@@ -11,5 +32,6 @@ setup(
     license='MIT',
     author='peco2282',
     author_email='pecop2282@gmail.com',
-    description='An APIwrapper for slack with python.'
+    description='An APIwrapper for slack with python.',
+    long_description=long_description
 )
